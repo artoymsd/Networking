9500890892dc7ddecbd34bc6e1bedb0e1d87eb61
Artem Sidorenko <artoymsd@gmail.com>
2019-10-06 11:33:12 +0300

    Initial Commit

---
+++ Networking/Networking.h
@@ -0,0 +1,19 @@
+//
+//  Networking.h
+//  Networking
+//
+//  Created by Artem Sidorenko on 06/10/2019.
+//  Copyright © 2019 Artem Sidorenko. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Networking.
+FOUNDATION_EXPORT double NetworkingVersionNumber;
+
+//! Project version string for Networking.
+FOUNDATION_EXPORT const unsigned char NetworkingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Networking/PublicHeader.h>
+
+
